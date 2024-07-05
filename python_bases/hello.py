f5ea316508579f3b9bef1621375726257d16629b
Victor Pitangui <pitanguigit@gmail.com>
2024-07-05 13:51:28 -0300

    Adicionar a biblioteca os

--- python_bases/hello.py
+++ python_bases/hello.py
@@ -17,5 +17,21 @@ Execução:
 """
 __version__ = "0.0.1"
 __author__ = "Victor Pitangui"
+__license__ = "Unlicense"
 
-print("Hello")
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "¡Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+
+print(msg)

00009814db4cc19c541533ed0a80c2c4a2566a80
Victor Pitangui <pitanguigit@gmail.com>
2024-07-09 16:30:19 -0300

    Adicionar formatação de texto

--- python_bases/tabuada.py
+++ python_bases/tabuada.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """Imprime a tabuada de 1 ao 10"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Victor Pitangui"
 
 numeros = list(range(1, 11))
 
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("-------------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)
 
